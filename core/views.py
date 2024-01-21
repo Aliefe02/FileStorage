8282c558728af97a897c412a5cb5d433de1d2e13
Ali EFE <78353929+Aliefe02@users.noreply.github.com>
2024-01-21 19:23:10 +0300

    Update views.py

--- core/views.py
+++ core/views.py
@@ -8,7 +8,7 @@ from django.http import FileResponse
 
 def index(request):
     #files_folder_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'Files')
-    files_folder_path = 'C:\\Users\\ali_e\\Masaüstü\\Project\\File_Storage\\Files'
+    files_folder_path = '\\Files'    # Modify this part
     # Get the list of files in the "Files" folder
     files_list = os.listdir(files_folder_path)
 
@@ -23,7 +23,7 @@ def upload(request):
         uploaded_file = request.FILES['file']
         
         # Specify the path where you want to save the uploaded files
-        upload_folder = 'C:\\Users\\ali_e\\Masaüstü\\Project\\File_Storage\\Files'
+        upload_folder = '\\Files'    # Modify this part
 
         # Save the file to the specified path
         with open(os.path.join(upload_folder, uploaded_file.name), 'wb+') as destination:
@@ -34,6 +34,6 @@ def upload(request):
     return redirect('uploadPage')
 
 def download(request, Filename=''):
-    os.chdir('C:\\Users\\ali_e\\Masaüstü\\Project\\File_Storage\\Files')
+    os.chdir('\\Files')    # Modify this part
     request_file= open(Filename, 'rb')
     return FileResponse(request_file)
